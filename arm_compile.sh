c73c986aa548ce0791d59dcbf62d7822538972b9
Jim <1150660744@qq.com>
2021-08-03 17:42:55 +0800

    first version
    Signed-off-by: Jim <1150660744@qq.com>

---
+++ arm_compile.sh
@@ -0,0 +1,2 @@
+/home/jim/workspace/android/king_rp_rk3399_DDR3_8.1_SDK-20200716/prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc memtester.c tests.c -static -o arm_mem_test
+./memtester -p 0x0c0000 64k
